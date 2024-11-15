988fe006c60f1513ef1fab736683266bdb1cd6c1
Angel Peralta <106847324+AngelFPeralta@users.noreply.github.com>
2024-11-15 13:06:27 -0400

    updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2540966458"
+    serviceWorkerVersion: "2679709599"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fe630b1d88c851ed624fbec12136a866",
+const RESOURCES = {"flutter_bootstrap.js": "8778dfbabc501167ab2b323d4dc9e029",
 "version.json": "e30dcb8a0b20339c5ad0b0e2104df5f1",
 "index.html": "dab904ee43429848191be0a30807c961",
 "/": "dab904ee43429848191be0a30807c961",
-"main.dart.js": "8fd232ed4d7f3e3e73a4cfb2fd8def83",
+"main.dart.js": "d8a56e3c9be5055bbf38116d32a7d244",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
